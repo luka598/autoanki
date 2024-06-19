3beb0b77b5ab5e098056c3ec1983ea557d19ea4f
luka598 <luka598@tuta.io>
2024-06-19 20:21:28 +0200

    - update prompt - cancellable image add thingy

--- generate.py
+++ generate.py
@@ -1,9 +1,8 @@
 from autoanki.gpt import send, Request, Message, TextContent, ImageUrlContent
 from autoanki.img import get_image
-from autoanki.fc import loads
 
 PROMPT = """\
-Please create 5-20 concise question type Anki flashcards based on the most important information for a high-school student from the provided lecture. Use the following three examples as general guidelines for the desired format:
+Please create 5-20 concise question type Anki flashcards based on the most important information for a high-school student from the provided lecture. Bolded, underlined or higlighted text is usually more important than other parts of the text. On the back of each flashcard, if relevant, feel free to use a semicolon and then include any relevant supplementary information (such as definitions, visual aids, high-yield facts, correlations, mnemonics or other relevant information). Flashcards must be in Croatian. Use the following three examples as general guidelines for the desired format:
 
 FRONT: For what sense is olfactory nerve responsible?
 BACK: Smell; It is the 1st cranial nerve.
@@ -13,8 +12,6 @@ BACK: Chromosomes are organized structures of DNA and proteins found in the nucl
 
 FRONT: Na koja područja se dijeli teorija književnosti?
 BACK: Stilistika, verzifikacija (znanost o stihu), klasifikacija književnosti.
-
-On the back of each flashcard, if relevant, feel free to use a semicolon and then include any relevant supplementary information (such as definitions, visual aids, high-yield facts, correlations, mnemonics or other relevant information). Flashcards must be in Croatian.
 """
 
 messages = [
@@ -28,20 +25,23 @@ if gt := input("General topic: "):
 
 images = 0
 while input(f"images: {images} | end? (y/N): ") != "y":
-    img = get_image(False)
-    if not img:
-        print("No image, exiting!")
-        exit()
+    try:
+        img = get_image(False)
+        if not img:
+            print("No image, exiting!")
+            exit()
 
-    if input("detail (L/h): ") != "h":
-        detail = "low"
-    else:
-        detail = "high"
+        if input("detail (L/h): ") != "h":
+            detail = "low"
+        else:
+            detail = "high"
 
-    messages.append(
-        Message("user", ImageUrlContent.from_image(img, "png", detail)),
-    )
-    images += 1
+        messages.append(
+            Message("user", ImageUrlContent.from_image(img, "png", detail)),
+        )
+        images += 1
+    except KeyboardInterrupt:
+        pass
 
 
 print("Sending request")
